1daa22687b2d5561c2db60f20e9ead59ee339b27
Christopher Etges <c.etges86@gmail.com>
2018-04-13 14:33:04 -0600

    getting started

--- liri.js
+++ liri.js
@@ -1,4 +1,34 @@
 require("dotenv").config();
+const request = require("request");
+const codes = require("./keys.js")
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+const userInput = process.argv[2];
+
+switch(userInput){
+    case "my-tweets":
+    //call twitterAPI to GET 20 more recent tweets with timestamps
+    let queryURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=bootcamp_etges&count=20"
+    request(queryURL, function(error,response,body){
+        if(!error && response.statusCode === 200){
+            
+            console.log(JSON.stringify(body,null,2));
+        } else {
+            console.log(error)
+        }
+    });
+    
+    break;
+
+    case "spotify-this-song":
+    let songName = process.argv[3];
+
+    //search spotify API for a song name to give Artist, song name back, link of the song on spotify, album that song is from
+    break;
+
+    case "movie-this":
+    let movieName = process.arg[3];
+    //omdb request for movie
+}
