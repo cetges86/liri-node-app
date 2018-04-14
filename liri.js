240bd1b434719ebc3f243216784e809337d03927
Christopher Etges <c.etges86@gmail.com>
2018-04-13 21:11:40 -0600

    addedtwitter spotify omdb

--- liri.js
+++ liri.js
@@ -1,34 +1,93 @@
 require("dotenv").config();
+const inquirer = require("inquirer");
 const request = require("request");
-const codes = require("./keys.js")
+const keys = require("./keys.js");
 
-// var spotify = new Spotify(keys.spotify);
+const Twitter = require("twitter");
+const Spotify = require("node-spotify-api");
+
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 const userInput = process.argv[2];
 
-switch(userInput){
+switch (userInput) {
     case "my-tweets":
-    //call twitterAPI to GET 20 more recent tweets with timestamps
-    let queryURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=bootcamp_etges&count=20"
-    request(queryURL, function(error,response,body){
-        if(!error && response.statusCode === 200){
-            
-            console.log(JSON.stringify(body,null,2));
-        } else {
-            console.log(error)
-        }
-    });
-    
-    break;
+        //call twitterAPI to GET 20 more recent tweets with timestamps
+        var params = {
+            screen_name: 'bootcamp_etges',
+            count: 20
+        };
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (!error) {
+                tweets.forEach(function (userTweets) {
+                    console.log("-------------------")
+                    console.log(userTweets.created_at);
+                    console.log(userTweets.text);
+                    console.log("-------------------")
+                })
+            } else {
+                console.log(error)
+            }
+        });
+
+        break;
 
     case "spotify-this-song":
-    let songName = process.argv[3];
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "song",
+                message: "What song would you like to search for?"
+            }
+        ]).then(function (answers) {
+            console.log(answers.song)
+            spotify.search({ type: 'track', query: answers.song, limit: 4 }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
 
-    //search spotify API for a song name to give Artist, song name back, link of the song on spotify, album that song is from
-    break;
+                data.tracks.items.forEach(function (songInfo) {
+                    console.log("-------------------");
+                    console.log(`Artist: ${songInfo.album.artists[0].name}`);
+                    console.log(`Song Name: ${songInfo.name}`);
+                    console.log(`Album Name: ${songInfo.album.name}`);
+                    console.log(`Spotify Link: ${songInfo.album.external_urls.spotify}`);
+                    console.log("-------------------");
+
+                })
+            });
+        })
+
+        //search spotify API for a song name to give Artist, song name back, link of the song on spotify, album that song is from
+        break;
 
     case "movie-this":
-    let movieName = process.arg[3];
-    //omdb request for movie
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "What movie would you like to search for?"
+            }
+        ]).then(function (answers) {
+            //omdb request for movie
+            if (answers.title === "") {
+                answers.title = "Mr. Nobody";
+            }
+            request(`http://www.omdbapi.com/?t=${answers.title}&y=&plot=short&apikey=trilogy`, function (error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+                    console.log("-------------------")
+                    console.log(JSON.parse(body).Title);
+                    console.log(JSON.parse(body).Year);
+                    console.log("imdb Rating: " + JSON.parse(body).Ratings[0].Value);
+                    console.log("Rotten Tomatoes Score: " + JSON.parse(body).Ratings[1].Value);
+                    console.log(JSON.parse(body).Country);
+                    console.log(JSON.parse(body).Language);
+                    console.log(JSON.parse(body).Plot);
+                    console.log(JSON.parse(body).Actors);
+                    console.log("-------------------")
+                }
+            });
+        })
 }
